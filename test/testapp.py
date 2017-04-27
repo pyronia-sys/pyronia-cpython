2e35305325a7d674139b3810f131ce21152b6a43
Marcela Melara <msmelara@gmail.com>
2017-04-27 18:05:59 -0400

    Debugging native frame hack prototype

---
+++ test/testapp.py
@@ -0,0 +1,19 @@
+import os
+
+os.system("echo hello!")
+
+# this will actually execute an `ls` command
+import attacklib
+
+# this will actually replace the argument in the stack frame
+attacklib.newprint("hello, world")
+
+# this will actually change the current line number in the source code
+attacklib.coolprint("hello, world")
+
+# this will actually use attacklib's os.system
+os.system("echo hello!")
+
+from attacklib_native import *
+
+native_print("YOOOOO")
